d4e6ff4abdee54ed3ad7baf88952178521568b96
Gung Dika <gungdika88@gmail.com>
2024-11-29 10:28:25 +0800

    Fixing button slider

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -32,12 +32,12 @@ import 'swiper/css/pagination'
   border-color: rgb(79 70 229 / var(--tw-border-opacity)) !important;
 }
 
-.swiper-slide.swiper-slide-active > .swiper-slide-active\:text-indigo-600 {
+.swiper-slide.swiper-slide-active>.swiper-slide-active\:text-indigo-600 {
   --tw-text-opacity: 1;
   color: rgb(79 70 229 / var(--tw-text-opacity));
 }
 
-.swiper-slide.swiper-slide-active > .flex .grid .swiper-slide-active\:text-indigo-600 {
+.swiper-slide.swiper-slide-active>.flex .grid .swiper-slide-active\:text-indigo-600 {
   --tw-text-opacity: 1;
   color: rgb(79 70 229 / var(--tw-text-opacity));
 }
@@ -47,96 +47,57 @@ import 'swiper/css/pagination'
   <section class="p-16 mt-10 relative">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div
-        class="mb-14 flex flex-col justify-center items-center sm:flex-row sm:items-center sm:justify-between max-sm:gap-8"
-      >
+        class="mb-14 flex flex-col justify-center items-center sm:flex-row sm:items-center sm:justify-between max-sm:gap-8">
         <h2 class="text-4xl text-center font-bold text-gray-900 lg:text-left">Testimonials</h2>
         <!-- Slider controls -->
         <div class="flex items-center gap-8">
-          <button
-            id="slider-button-left"
+          <button id="slider-button-left"
             class="swiper-button-prev group flex justify-center items-center border border-solid border-indigo-600 w-12 h-12 transition-all duration-500 rounded-full hover:bg-indigo-600"
-            data-carousel-prev
-          >
-            <svg
-              class="h-6 w-6 text-indigo-600 group-hover:text-white"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+            data-carousel-prev>
+            <svg class="h-6 w-6 text-indigo-600 group-hover:text-white" viewBox="0 0 24 24" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M20.9999 12L4.99992 12M9.99992 6L4.70703 11.2929C4.3737 11.6262 4.20703 11.7929 4.20703 12C4.20703 12.2071 4.3737 12.3738 4.70703 12.7071L9.99992 18"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
           </button>
-          <button
-            id="slider-button-right"
+          <button id="slider-button-right"
             class="swiper-button-next group flex justify-center items-center border border-solid border-indigo-600 w-12 h-12 transition-all duration-500 rounded-full hover:bg-indigo-600"
-            data-carousel-next
-          >
-            <svg
-              class="h-6 w-6 text-indigo-600 group-hover:text-white"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+            data-carousel-next>
+            <svg class="h-6 w-6 text-indigo-600 group-hover:text-white" viewBox="0 0 24 24" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M3 12L19 12M14 18L19.2929 12.7071C19.6262 12.3738 19.7929 12.2071 19.7929 12C19.7929 11.7929 19.6262 11.6262 19.2929 11.2929L14 6"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
           </button>
         </div>
       </div>
     </div>
     <div class="container">
-      <swiper-container
-        pagination-el=".swiper-pagination"
-        class="review-slider max-w-screen-lg"
-        effect="coverflow"
-        :grab-cursor="true"
-        :centered-slides="true"
-        :loop="true"
-        slides-per-view="auto"
-        coverflow-effect-rotate="0"
-        coverflow-effect-stretch="0"
-        coverflow-effect-depth="100"
-        coverflow-effect-modifier="2.5"
-        :pagination-clickable="true"
-        space-between="200"
-        :initial-slide="middleNumber"
-      >
+      <swiper-container pagination-el=".swiper-pagination" navigation-next-el=".swiper-button-next"
+        navigation-prev-el=".swiper-button-prev" class="review-slider max-w-screen-lg" effect="coverflow"
+        :grab-cursor="true" :centered-slides="true" :loop="true" slides-per-view="auto" coverflow-effect-rotate="0"
+        coverflow-effect-stretch="0" coverflow-effect-depth="100" coverflow-effect-modifier="2.5"
+        :pagination-clickable="true" space-between="200" :initial-slide="middleNumber">
         <swiper-slide class="review-slide" v-for="review in reviews" :key="review.id">
           <div class="swiper-wrapper">
             <div
-              class="swiper-slide group bg-white border border-solid h-auto border-gray-300 rounded-2xl p-6 transition-all duration-500 w-full hover:border-indigo-600 slide-active:border-indigo-600"
-            >
+              class="swiper-slide group bg-white border border-solid h-auto border-gray-300 rounded-2xl p-6 transition-all duration-500 w-full hover:border-indigo-600 slide-active:border-indigo-600">
               <div
-                class="flex items-center mb-9 gap-2 text-amber-500 transition-all duration-500 group-hover:text-indigo-600 swiper-slide-active:text-indigo-600"
-              >
+                class="flex items-center mb-9 gap-2 text-amber-500 transition-all duration-500 group-hover:text-indigo-600 swiper-slide-active:text-indigo-600">
                 <StarRating :style="100" :rating="review.rating"></StarRating>
               </div>
               <p
-                class="text-lg text-gray-500 leading-8 h-24 transition-all duration-500 mb-9 group-hover:text-gray-800"
-              >
+                class="text-lg text-gray-500 leading-8 h-24 transition-all duration-500 mb-9 group-hover:text-gray-800">
                 {{ review.review }}
               </p>
               <div class="flex items-center gap-5">
-                <img
-                  class="rounded-full object-cover"
-                  src="https://pagedone.io/asset/uploads/1696229969.png"
-                  alt="avatar"
-                />
+                <img class="rounded-full object-cover" src="https://pagedone.io/asset/uploads/1696229969.png"
+                  alt="avatar" />
                 <div class="grid gap-1">
                   <h5
-                    class="text-gray-900 font-medium transition-all duration-500 group-hover:text-indigo-600 swiper-slide-active:text-indigo-600"
-                  >
+                    class="text-gray-900 font-medium transition-all duration-500 group-hover:text-indigo-600 swiper-slide-active:text-indigo-600">
                     {{ review.name }}
                   </h5>
                 </div>
